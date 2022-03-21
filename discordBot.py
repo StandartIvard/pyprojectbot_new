13ad33dd69513b89b327b5f8cb603d5b8b4378eb
Abracadabra000 <artemachernov@gmail.com>
2022-03-21 18:03:41 +0300

    discord bot added

--- discordBot.py
+++ discordBot.py
@@ -1,49 +1,49 @@
 import discord
 from discord.ext import commands
 
-bot = commands.Bot(command_prefix='!')
+
+class BotsCog(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(name='test')
+    async def test(self, ctx):
+        await ctx.send('What???')
+
+    @commands.command(name='points')
+    async def points(self, ctx):
+        await ctx.send(ctx.message.author.mention + ' у тебя ' + str(bot.users_list[str(ctx.message.author)]) + ' очков!')
+
+
+class DiscordBot(commands.Bot):
+    def __init__(self, *args, **kwargs):
+        self.users_list = {}
+        super().__init__(*args, **kwargs)
+
+    async def on_ready(self):
+        for g in bot.guilds:
+            for c in g.text_channels:
+                await c.send('here i am')
+            for m in g.members:
+                if m.name not in self.users_list:
+                    self.users_list[str(m)] = 0
+
+    async def on_message(self, mes):
+        if mes.author == bot.user:
+            return
+        if str(mes.author) not in self.users_list:
+            self.users_list[str(mes.author)] = 0
+        self.users_list[str(mes.author)] += 1
+        if mes.content.startswith('!'):
+            await bot.process_commands(mes)
+
+    async def on_member_join(self, member):
+        print(member)
+
+
+bot = DiscordBot(command_prefix='!')
+bot.add_cog(BotsCog(bot))
+
 TOKEN = 'OTUxNDgzNDEzNDk4NTc2OTg2.YioH-w.uJDuDQNA_uz8EvizbpsxpfHrH-Q'
-
-users = {}
-
-@bot.event
-async def on_ready():
-    for g in bot.guilds:
-        for c in g.text_channels:
-            await c.send('here i am')
-        for m in g.members:
-            if m.name not in users:
-                users[str(m)] = 0
-    print(users)
-
-@bot.command(name='test')
-async def test(ctx):
-    for guild in bot.guilds:
-        for member in guild.members:
-            print(member)
-            users[str(member)] = 0
-    print(users)
-    await ctx.send('What???')
-
-@bot.command()
-async def очки(ctx):
-    await ctx.send(ctx.message.author.mention + ' у тебя ' + str(users[str(ctx.message.author)]) + ' очков!')
-
-"""@bot.event
-async def on_message(mes):
-    pass
-    if mes.author == bot.user:
-        return
-    users[str(mes.author)] += 1
-
-    if mes.content[0] != '!':
-        pass
-    else:
-        await bot.process_commands(mes)"""
-
-@bot.event
-async def on_member_join(member):
-    users[str(member.author)] = 0
-    print(users)
 
 bot.run(TOKEN)
