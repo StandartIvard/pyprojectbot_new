21d3e19e43cca839b14fe7078d7b2319ca87e5a2
artem <ivard_iv@mail.ru>
2022-05-04 20:49:24 +0300

    commit a lot of

--- discordBot.py
+++ discordBot.py
@@ -283,6 +283,4 @@ bot.add_cog(BotsCog(bot))
 
 bot.loop.create_task(bot.send_on_timer('bot_talking', messagesFile.discord_messages))
 
-loop = asyncio.get_event_loop()
-loop.run_until_complete(bot.run(TOKEN))
-loop.close()
+bot.run(TOKEN)
